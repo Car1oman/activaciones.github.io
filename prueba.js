df4430449141de46dc187a25a86896b299971c53
Car1oman <121407142+Car1oman@users.noreply.github.com>
2024-04-19 08:52:59 -0500

    Update prueba.js
    update key

--- prueba.js
+++ prueba.js
@@ -1,3 +1,3 @@
-const array = 'k1MNLrFBxph-q9yfliDiNdE_skABCenECySazIA';
+const array = '8Jz0dFdB9_h5SdUOs5gFbUjt2P7tojoZBySazIA';
 const newArray = array.split('').reverse().join('');
 export default newArray;
